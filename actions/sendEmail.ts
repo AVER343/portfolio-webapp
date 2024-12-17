ad739fa177dde73d25601066579997d4e9238f3e
aver343 <ankushverma0609@gamil.com>
2024-12-17 16:52:45 +0530

    Resend API update

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -31,7 +31,7 @@ export const sendEmail = async (formData: FormData) => {
       from: "Contact Form <onboarding@resend.dev>",
       to: "ankushvermagb@gmail.com",
       subject: "Message from contact form",
-      reply_to: senderEmail,
+      replyTo: senderEmail,
       react: React.createElement(ContactFormEmail, {
         message: message,
         senderEmail: senderEmail,
